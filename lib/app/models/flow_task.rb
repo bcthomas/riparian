cb9292f8ad84aa04c029396cfdde2838c460c87c
Andrea Singh <andrea.singh@berkeley.edu>
2013-04-25 10:01:40 -0700

    add started_at to list of accessible attributes

--- lib/app/models/flow_task.rb
+++ lib/app/models/flow_task.rb
@@ -1,5 +1,5 @@
 class FlowTask < ActiveRecord::Base
-  attr_accessible :type, :redirect_url, :options, :inputs_attributes, :finished_at, :error_msg
+  attr_accessible :type, :redirect_url, :options, :inputs_attributes, :finished_at, :started_at, :error_msg
   belongs_to :user
   
   # Input resources for the task, can be any model instance or file attachment
